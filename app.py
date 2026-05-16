51cec7d104e3f0349607c54c6b5dc139ab0048f7
belkovaav18-cyber <belkova.av18@physics.msu.ru>
2026-05-16 17:10:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 from sheets import load_guests, save_results
 from preprocess import preprocess_guests
-from solver import solve
+from solver_controller import smart_solve
 
 
 SHEET_ID = "1lF4SV24wTo5OwsidQ7UPqBVaGzdw_fBSx0OuBJJ4cWg"
@@ -14,35 +14,39 @@ st.title("🏨 Расселение")
 rooms_df = pd.read_excel("data/rooms.xlsx")
 rooms = rooms_df.to_dict("records")
 
-st.subheader("Комнаты")
-st.dataframe(rooms_df)
-
 raw = load_guests(SHEET_ID, TAB)
 guests_df = preprocess_guests(raw)
 
+# -------------------------
+# split
+# -------------------------
 non_residents = guests_df[guests_df["resident"] == False].copy()
-guests_for_solver = guests_df[guests_df["resident"] == True].copy()
+residents = guests_df[guests_df["resident"] == True].copy()
 
 st.subheader("Гости")
 st.dataframe(guests_df)
 
 if st.button("🚀 Расселить"):
 
-    result = solve(
-        guests_for_solver.to_dict("records"),
+    result, debug = smart_solve(
+        residents.to_dict("records"),
         rooms
     )
 
-    non_res = non_residents.copy()
-    non_res["room_id"] = "не проживает"
+    # add non-residents
+    non_residents_result = non_residents.copy()
+    non_residents_result["room_id"] = "не проживает"
 
     final_result = pd.concat([
         result,
-        non_res[["fio", "room_id"]]
+        non_residents_result[["fio", "room_id"]]
     ])
 
     st.subheader("Result")
     st.dataframe(final_result)
+
+    st.subheader("Debug")
+    st.json(debug)
 
     save_results(SHEET_ID, "Result", final_result)
 
