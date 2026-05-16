3ef916476e09b605d80e849007ab2107b2d9a195
belkovaav18-cyber <belkova.av18@physics.msu.ru>
2026-05-16 12:19:40 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# штрафы (можно крутить как регуляторы системы)
+
+WEIGHTS = {
+    "gender_conflict": 1000,
+    "age_diff": 2,
+    "status_diff": 3,
+    "city_diff": 1,
+    "group_split": 500,
+}
