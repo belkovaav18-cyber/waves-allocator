4076b50032f3a7089fd1aeb1534ad8511837cb18
belkovaav18-cyber <belkova.av18@physics.msu.ru>
2026-05-16 08:05:44 +0300

    Update google_sheets.py

--- utils/google_sheets.py
+++ utils/google_sheets.py
@@ -32,3 +32,31 @@ def load_guests_from_gsheet():
     data = worksheet.get_all_records()
 
     return pd.DataFrame(data)
+def save_allocations_to_gsheet(df, sheet_name="Расселение"):
+
+    credentials_info = dict(st.secrets["gcp_service_account"])
+
+    if "private_key" in credentials_info:
+        credentials_info["private_key"] = credentials_info["private_key"].replace("\\n", "\n")
+
+    creds = Credentials.from_service_account_info(
+        credentials_info,
+        scopes=[
+            "https://www.googleapis.com/auth/spreadsheets",
+            "https://www.googleapis.com/auth/drive"
+        ]
+    )
+
+    client = gspread.authorize(creds)
+
+    sh = client.open_by_key("10cBNkDQ3fOCajBIjeAsaCPsivEfVShGZ-BHmLcC6l5s")
+
+    # пробуем открыть лист
+    try:
+        worksheet = sh.worksheet(sheet_name)
+        worksheet.clear()
+    except:
+        worksheet = sh.add_worksheet(title=sheet_name, rows="1000", cols="20")
+
+    # записываем данные
+    worksheet.update([df.columns.values.tolist()] + df.values.tolist())
