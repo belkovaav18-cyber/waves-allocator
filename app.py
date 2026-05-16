c28c5e44f0b0474a33138753164738c99754ed1c
belkovaav18-cyber <belkova.av18@physics.msu.ru>
2026-05-16 12:43:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,34 +5,70 @@ from sheets import load_guests, save_results
 from preprocess import preprocess
 from solver import solve
 
+
 SHEET_ID = "1lF4SV24wTo5OwsidQ7UPqBVaGzdw_fBSx0OuBJJ4cWg"
 TAB = "Sheet"
 
+st.set_page_config(layout="wide")
+
+st.title("🏨 Система расселения")
+
+
+# ---------------------------------------------------
+# LOAD ROOMS
+# ---------------------------------------------------
+rooms = pd.read_excel(
+    "data/rooms.xlsx",
+    engine="openpyxl"
+)
+
+st.subheader("Комнаты")
+st.dataframe(rooms)
+
+
+# ---------------------------------------------------
+# LOAD GUESTS
+# ---------------------------------------------------
 raw = load_guests(SHEET_ID, TAB)
-
-
-
-st.title("🏨 OR-Tools расселение")
-
-rooms = pd.read_excel("data/rooms.xlsx")
-
 
 guests = preprocess(raw)
 
 st.subheader("Гости")
 st.dataframe(guests)
 
-if st.button("🚀 Оптимизировать расселение"):
 
-    result = solve(guests, rooms)
-
-    st.subheader("Результат")
-    st.dataframe(result)
-
-    result.to_excel("data/output.xlsx", index=False)
+# ---------------------------------------------------
+# BUTTON
+# ---------------------------------------------------
+if st.button("🚀 Расселить"):
 
     try:
-        save_results(SHEET_ID, "Result", result)
-        st.success("Сохранено в Google Sheets")
+
+        result = solve(guests, rooms)
+
+        st.subheader("Result")
+
+        st.dataframe(result)
+
+        # -----------------------------------------
+        # excel
+        # -----------------------------------------
+        result.to_excel(
+            "data/result.xlsx",
+            index=False
+        )
+
+        # -----------------------------------------
+        # google sheets
+        # -----------------------------------------
+        save_results(
+            SHEET_ID,
+            "Result",
+            result
+        )
+
+        st.success("Готово")
+
     except Exception as e:
-        st.error(str(e))
+
+        st.exception(e)
