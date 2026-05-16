87139d47b4fe8c33d37e7a32bcafa3c437a458ce
belkovaav18-cyber <belkova.av18@physics.msu.ru>
2026-05-16 08:01:30 +0300

    Update google_sheets.py

--- utils/google_sheets.py
+++ utils/google_sheets.py
@@ -1,20 +1,34 @@
 import pandas as pd
 import streamlit as st
-from google.oauth2 import service_account
 import gspread
+from google.oauth2.service_account import Credentials
+
+
+SPREADSHEET_ID = "10cBNkDQ3fOCajBIjeAsaCPsivEfVShGZ-BHmLcC6l5s"
+SHEET_NAME = "регистрация"
 
 
 def load_guests_from_gsheet():
 
-    creds = service_account.Credentials.from_service_account_info(
-        st.secrets["gcp_service_account"]
+    credentials_info = dict(st.secrets["gcp_service_account"])
+
+    # FIX private key
+    if "private_key" in credentials_info:
+        credentials_info["private_key"] = credentials_info["private_key"].replace("\\n", "\n")
+
+    creds = Credentials.from_service_account_info(
+        credentials_info,
+        scopes=[
+            "https://www.googleapis.com/auth/spreadsheets",
+            "https://www.googleapis.com/auth/drive"
+        ]
     )
 
     client = gspread.authorize(creds)
 
-    # Открываем таблицу по названию
-    sheet = client.open("регистрация").sheet1
+    sh = client.open_by_key(SPREADSHEET_ID)
+    worksheet = sh.worksheet(SHEET_NAME)
 
-    data = sheet.get_all_records()
+    data = worksheet.get_all_records()
 
     return pd.DataFrame(data)
