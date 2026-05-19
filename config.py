db1d47964ab02da5401c13fc047bafea212fe5ee
otinov&a.co <belkova.av18@physics.msu.ru>
2026-05-19 14:37:15 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,24 @@
-# штрафы (можно крутить как регуляторы системы)
+# config.py
 
+# Веса для штрафов в solver
 WEIGHTS = {
-    "gender_conflict": 1000,
-    "age_diff": 2,
-    "status_diff": 3,
-    "city_diff": 1,
-    "group_split": 500,
+    "gender_conflict": 1000,  # Самый большой штраф - нельзя мужчин с женщинами
+    "city_diff": 50,
+    "status_diff": 100,  # Штраф за разный статус (студент vs профессор)
 }
+
+# Статусы и их приоритеты для совместимости
+STATUS_HIERARCHY = {
+    'student': 1,
+    'postgraduate': 2,
+    'assistant': 3,
+    'docent': 4,
+    'professor': 5,
+    'academician': 5
+}
+
+# Максимальная допустимая разница в статусе для совместного проживания
+MAX_STATUS_DIFFERENCE = 2
+
+# Максимальная допустимая разница в возрасте для совместного проживания
+MAX_AGE_DIFFERENCE = 10
